63810b5c29e8821d088e60f28d1e9367f7be6a19
AaronKWalker <aaron.walker@outlook.com>
2017-07-11 09:17:02 -0500

    initial push

---
+++ testing/test.js
@@ -0,0 +1,18 @@
+// var apiKey = ;
+
+
+var VAqueryURL ="https://developer.nps.gov/api/v0/parks?parkCode=yell";
+$("#apiCall").on("click", function() {
+  $.ajax({
+    url: VAqueryURL,
+    method: "GET",
+    headers:{
+      Authorization: "E8FE49A8-5724-4DE5-B997-F6D79162EDDC"
+    },
+    datatype: "jsonp"
+    // crossorigin:true
+  })
+  .done(function(response) {
+    console.log(response);
+  });
+});
